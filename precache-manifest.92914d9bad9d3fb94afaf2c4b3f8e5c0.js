371512602d0680571bbfd0f005e8fe1bb2c6dac2
KayinCheung <cheungkayin00@gmail.com>
2019-11-14 09:35:25 +0800

    init

---
+++ precache-manifest.92914d9bad9d3fb94afaf2c4b3f8e5c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e77a254d5efbd5b57c90b648ba0d0d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "795bf10e6f838d128ba7",
+    "url": "/static/css/2.1e6845a9.chunk.css"
+  },
+  {
+    "revision": "3cbe7c654358f0801938",
+    "url": "/static/css/main.739e6324.chunk.css"
+  },
+  {
+    "revision": "795bf10e6f838d128ba7",
+    "url": "/static/js/2.5a937999.chunk.js"
+  },
+  {
+    "revision": "3cbe7c654358f0801938",
+    "url": "/static/js/main.a8c57cb6.chunk.js"
+  },
+  {
+    "revision": "b8d9006b1975b0fa6578",
+    "url": "/static/js/runtime-main.02d02f4e.js"
+  }
+]);
